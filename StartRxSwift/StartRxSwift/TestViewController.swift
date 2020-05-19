768d1f54d2c3aac4c7f9c26cdebb7584e391ee8d
InjunPark <demianjun@gmail.com>
2020-05-20 02:30:38 +0900

    study: about sugar API

--- StartRxSwift/StartRxSwift/TestViewController.swift
+++ StartRxSwift/StartRxSwift/TestViewController.swift
@@ -9,16 +9,32 @@
 import UIKit
 import RxSwift
 
-class ViewController: UIViewController {
+class TestViewController: UIViewController {
   
   let url = "http://www.google.com"
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+    testSubscribe()
   }
   
   // MARK: 비동기로 생기는 데이터를 Observable로 감싸서 리턴하는 방법
+  //func testJson(_ url: String) -> Observable<String?> {
+  //  return Observable.create { testData in
+  //    DispatchQueue.global().async {
+  //      let url = URL(string : url)!
+  //      guard let data = try? Data(contentsOf: url) else { return }
+  //      let json = String(data: data, encoding: .utf8)
+  //
+  //      DispatchQueue.main.async {
+  //        testData.onNext(json)
+  //        testData.onCompleted()
+  //      }
+  //    }
+  //    return Disposables.create()
+  //  }
+  //}
+  
   func testJson(_ url: String) -> Observable<String?> {
     return Observable.create { testData in
       let url = URL(string: url)!
@@ -26,21 +42,21 @@ class ViewController: UIViewController {
         if err != nil {
           testData.onError(err!)
         }
-        
+
         if let data = data, let json = String(data: data, encoding: .utf8) {
           testData.onNext(json)
         }
         testData.onCompleted()
       }
-      
+
       task.resume()
-      
+
       return Disposables.create() {
         task.cancel()
       }
     }
   }
-  
+
   // MARK: Observable로 오는 데이터를 받아서 처리하는 방버
   func testDataProcessing() {
    _ = testJson(url)
@@ -49,29 +65,29 @@ class ViewController: UIViewController {
         case let .next(json):
           guard let json = json else { return }
           print("\(json)")
-          
+
         case .completed: break
-          
+
         case .error: break
         }
     }
   }
   
+
+  func testObservable() -> Observable<String?> {
+    return Observable.just("asdjklfhasiduhfiasjklokjhgaskjgfauiweghjkasdnfhjkasgdfjkghasdjkifha")
+  }
+  
+  func testSubscribe() {
+    _ = testObservable()
+      .map { txt in txt?.count ?? 0 }
+      .filter { cnt in cnt > 0 }
+      .observeOn(MainScheduler.instance) // Dispatchqueue 대신 사용, operator라고 부름
+      .subscribe(onNext: {
+        print("글자수: \($0)")
+      })
+  }
 }
 
 
-//func testJson(_ url: String) -> Observable<String?> {
-//  return Observable.create { testData in
-//    DispatchQueue.global().async {
-//      let url = URL(string : url)!
-//      guard let data = try? Data(contentsOf: url) else { return }
-//      let json = String(data: data, encoding: .utf8)
-//
-//      DispatchQueue.main.async {
-//        testData.onNext(json)
-//        testData.onCompleted()
-//      }
-//    }
-//    return Disposables.create()
-//  }
-//}
+

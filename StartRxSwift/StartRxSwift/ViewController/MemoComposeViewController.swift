628367a50a397215450c975d253390d6f0fa4a5b
InjunPark <demianjun@gmail.com>
2020-06-24 17:55:06 +0900

    memo write

--- StartRxSwift/StartRxSwift/ViewController/MemoComposeViewController.swift
+++ StartRxSwift/StartRxSwift/ViewController/MemoComposeViewController.swift
@@ -7,9 +7,18 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Action
+import NSObject_Rx
+
 
 class MemoComposeViewController: UIViewController, ViewModelBindableType {
   var viewModel: MemoComposeViewModel!
+  
+  @IBOutlet weak var cancelButton: UIBarButtonItem!
+  @IBOutlet weak var saveButton: UIBarButtonItem!
+  @IBOutlet weak var contentTextView: UITextView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -17,6 +26,33 @@ class MemoComposeViewController: UIViewController, ViewModelBindableType {
   }
   
   func bindViewModel() {
+    viewModel.title
+      .drive(navigationItem.rx.title)
+      .disposed(by: rx.disposeBag)
     
+    viewModel.initialText
+      .drive(contentTextView.rx.text)
+      .disposed(by: rx.disposeBag)
+    
+    cancelButton.rx.action = viewModel.cancelAction
+    
+    saveButton.rx.tap
+      .throttle(.microseconds(500), scheduler: MainScheduler.instance)
+      .withLatestFrom(contentTextView.rx.text.orEmpty)
+      .bind(to: viewModel.saveAction.inputs)
+      .disposed(by: rx.disposeBag)
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    contentTextView.becomeFirstResponder()
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    if contentTextView.isFirstResponder {
+      contentTextView.resignFirstResponder()
+    }
   }
 }

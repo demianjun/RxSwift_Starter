77ca166b8d2a8eeb1cd3961c21ce34d368fa02dc
InjunPark <demianjun@gmail.com>
2020-06-24 13:58:32 +0900

    produce memory storage

---
+++ StartRxSwift/StartRxSwift/Service/MemoStorageType.swift
@@ -0,0 +1,25 @@
+//
+//  MemoStorageType.swift
+//  StartRxSwift
+//
+//  Created by Demian on 2020/06/24.
+//  Copyright © 2020 Demian. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+protocol MemoStorageType {
+  @discardableResult
+  func createMemo(content: String) -> Observable<Memo>
+  
+  @discardableResult
+  func memoList() -> Observable<[Memo]>
+  
+  @discardableResult
+  func update(memo: Memo, content: String) -> Observable<Memo>
+  
+  @discardableResult
+  func delete(memo: Memo) -> Observable<Memo>
+  
+}
